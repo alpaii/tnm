75533c27a6a30092bfe833c0d4d47215a6e847f8
jkh <jkh@example.com>
2025-01-02 13:48:42 +0900

    create account page

--- frontend/app/src/routes.js
+++ frontend/app/src/routes.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
+
+const Accounts = React.lazy(() => import('./views/money/accounts/Accounts'))
+
 const Colors = React.lazy(() => import('./views/theme/colors/Colors'))
 const Typography = React.lazy(() => import('./views/theme/typography/Typography'))
 
@@ -54,6 +57,8 @@ const Widgets = React.lazy(() => import('./views/widgets/Widgets'))
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
+  { path: '/money', name: 'Money', element: Accounts, exact: true },
+  { path: '/money/accounts', name: 'Accounts', element: Accounts },
   { path: '/theme', name: 'Theme', element: Colors, exact: true },
   { path: '/theme/colors', name: 'Colors', element: Colors },
   { path: '/theme/typography', name: 'Typography', element: Typography },
